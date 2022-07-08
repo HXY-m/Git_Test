8529f15eaab5defca0174dcb97217e1f3c90455a
HXY <2195682742@qq.com>
2022-07-08 17:20:23 +0800

    Master Commit

--- src/main/java/com/hxy/git/HelloWorld.java
+++ src/main/java/com/hxy/git/HelloWorld.java
@@ -8,6 +8,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,Git");
         System.out.println("Update!!!");
-        System.out.println("Again Update!!!");
+        System.out.println("Java C++");
     }
 }
