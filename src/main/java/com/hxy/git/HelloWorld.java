2b0449bc5405b588fcf7f3c27a3ddca5b01c6cf6
HXY <2195682742@qq.com>
2022-07-08 17:07:22 +0800

    First Commmit

---
+++ src/main/java/com/hxy/git/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.hxy.git;
+
+/**
+ * @author 扈肖宇
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git");
+    }
+}
