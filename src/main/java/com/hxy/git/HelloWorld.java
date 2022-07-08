45b5ca2ee7fdb802e1cb0182e666278b6815946a
HXY <2195682742@qq.com>
2022-07-08 17:09:42 +0800

    Second Commit

--- src/main/java/com/hxy/git/HelloWorld.java
+++ src/main/java/com/hxy/git/HelloWorld.java
@@ -7,5 +7,6 @@ package com.hxy.git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,Git");
+        System.out.println("Update!!!");
     }
 }
