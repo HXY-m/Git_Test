201852332ac3361e1b3ef05682f53d26ee68bd07
HXY <2195682742@qq.com>
2022-07-08 17:18:58 +0800

    Hot-Fix Commit

--- src/main/java/com/hxy/git/HelloWorld.java
+++ src/main/java/com/hxy/git/HelloWorld.java
@@ -8,6 +8,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,Git");
         System.out.println("Update!!!");
-        System.out.println("Again Update!!!");
     }
 }
